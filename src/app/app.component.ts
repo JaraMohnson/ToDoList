a9d4bdf7a017697d90172d2daa3e1b754bc32d15
JaraMohnson <jaramohnson@gmail.com>
2022-08-06 20:59:36 -0400

    first commit for to-do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToDo } from './todo'; 
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,83 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ToDo';
+  title = 'ToDo'; 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  tasks:ToDo[] = [
+    {
+      task:"Cook pets gourmet breakfast",
+      completed: false
+    },
+    {
+      task:"Wash dishes",
+      completed:false
+    },
+    {
+      task:"Grocery shop",
+      completed:false
+    },
+    {
+      task:"Do laundry",
+      completed:false
+    },
+    {
+      task:"Buy squishmallow",
+      completed:false
+    },
+    {
+      task:"Catch a firefly",
+      completed:false
+    },
+    {
+      task:"Buy dog new outfit",
+      completed:false
+    }
+  ]
+
+  
+
+  completeTask(choice:ToDo):void {
+    let index = this.tasks.findIndex(t => t.task == choice.task );
+    this.tasks[index].completed = true;
+  }
+
+  //just create a new item, and push it to your existing []
+  addTodo(form:NgForm):void{
+    let newToDo:ToDo = {
+      task: form.form.value.Task,
+      completed: false
+    };
+
+    this.tasks.push(newToDo);
+  }
+
+  //just loop through the tasks and check if ANY are incomplete; if so, 
+  // flip the boolean 
+  //no param needed, returns a bool so we can use ngIf 
+ TasksComplete():boolean{
+    let result:boolean = true; 
+    this.tasks.forEach((t:ToDo) =>{
+      if(t.completed == false){
+        result = false;
+      }
+    });
+    return result;
+  }
+
+  Remove(choice:ToDo):void{ 
+    //find it 
+    let index = this.tasks.findIndex(t => t.task == choice.task);
+    //remove it 
+    this.tasks.splice(index,1);
+  }
+
+  
+  
+
+
+
 }
